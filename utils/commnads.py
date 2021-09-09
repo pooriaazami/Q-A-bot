f1d45102d1cff3953e21b3fef29f4c0f006beca5
Pooria Azami <pooriaazami@gmail.com>
2021-09-09 19:47:54 +0430

    start command added

---
+++ utils/commnads.py
@@ -0,0 +1,9 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+
+
+def start(update:Update, callback: CallbackContext):
+    user = update.effective_user
+    bot = callback.bot
+
+    bot.send_message(user.id, "Welcome to Bitoks Q&A bot!")
