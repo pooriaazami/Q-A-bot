05b0469e6627083804a43a53be8d46db85943a9e
Pooria Azami <pooriaazami@gmail.com>
2021-09-09 19:39:16 +0430

    inital code to start bot added

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from telegram.ext import Updater
+
+
 def read_token():
     with open('token.txt') as file:
         token = file.read()
@@ -6,7 +9,12 @@ def read_token():
 
 
 def main():
-    pass
+    token = read_token()
+
+    updater = Updater()
+    dispatcher = updater.dispatcher
+
+    updater.start_polling()
 
 
 if __name__ == '__main__':
