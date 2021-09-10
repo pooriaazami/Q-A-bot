1577f07d3f48d67f4defa737f84e9c12a178e42b
Pooria Azami <pooriaazami@gmail.com>
2021-09-10 15:43:41 +0430

    singleton updated for user validation

--- utils/DataHolder.py
+++ utils/DataHolder.py
@@ -1,9 +1,13 @@
 class DataHolder:
     __instance = None
 
+    ADMIN = 2
+    USER = 1
+    INVALID_USERNAME = -1
+
     def __init__(self):
         if DataHolder.__instance is None:
-            pass
+            self.__registered_users = {}
 
     @staticmethod
     def get_instance():
@@ -11,3 +15,9 @@ class DataHolder:
             DataHolder.__instance = DataHolder()
 
         return DataHolder.__instance
+
+    def push_new_registered_user(self, username, roll):
+        self.__registered_users[username] = roll
+
+    def get_registered_user(self, username):
+        return self.__registered_users.get(username, DataHolder.INVALID_USERNAME)
